a8191eaae5c79e2fc0232338a9bcbc7591af500e
Lhwhatever <llinhong.2301+github@gmail.com>
2019-05-24 23:33:39 +0800

    Create directory for exceptions and create one exception class
    1. All future exceptions will be found under `src/except`.
    2. Create `ArityMismatchException` for when an operation receives an invalid number of arguments.

---
+++ src/except/arityMismatchException.cpp
@@ -0,0 +1,16 @@
+#include "arityMismatchException.h"
+
+#include <sstream>
+
+ArityMismatchException::ArityMismatchException(std::string identifier,
+                                               int expected, int received,
+                                               std::string extra)
+    : ID{identifier}, EXPECTED{expected}, RECEIVED{received}, EXTRA{extra} {};
+
+const char* ArityMismatchException::what() const noexcept {
+    std::ostringstream ss{};
+    ss << "Arity mismatch: operator " << ID << " expected " << EXPECTED
+       << " arguments but received " << RECEIVED << '.';
+    if (EXTRA != "") ss << "\nError at: " << EXTRA;
+    return ss.str().c_str();
+}
---
+++ src/except/arityMismatchException.h
@@ -0,0 +1,20 @@
+#ifndef CALC__EXCEPT__ARITY_MISMATCH_EXCEPTION_H_
+#define CALC__EXCEPT__ARITY_MISMATCH_EXCEPTION_H_
+
+#include <exception>
+
+class ArityMismatchException : public std::exception {
+    const std::string ID;
+    const int EXPECTED;
+    const int RECEIVED;
+    const std::string EXTRA;
+
+   public:
+    ArityMismatchException(std::string identifier, int argsExpected,
+                           int argsReceived, std::string extra = "");
+    ~ArityMismatchException();
+
+    virtual const char* what() const noexcept override;
+};
+
+#endif
