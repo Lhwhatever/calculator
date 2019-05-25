aad0439205f8e4464e8a495b859426a0fe8ea015
Lhwhatever <llinhong.2301+github@gmail.com>
2019-05-26 00:11:52 +0800

    Introduce division operator
    1. If division of two integer tokens yields an integer, an integer will be produced.
    2. Otherwise, a float will be produced.

--- src/packages/basePackage.cpp
+++ src/packages/basePackage.cpp
@@ -10,6 +10,8 @@ auto minus =
     std::make_shared<OperatorToken>("-", 2, 4, OperatorToken::LEFT_ASSOC);
 auto times =
     std::make_shared<OperatorToken>("*", 2, 5, OperatorToken::LEFT_ASSOC);
+auto divide =
+    std::make_shared<OperatorToken>("/", 2, 5, OperatorToken::LEFT_ASSOC);
 
 }  // namespace base_func
 
@@ -188,6 +190,89 @@ void multiply__ml(ValueStack& values) {
 
     values.push_back(std::make_shared<Tm>(*a * *b));
 }
+
+template <typename T>
+void divide__(ValueStack& values) {
+    auto b = std::static_pointer_cast<T>(values.back());
+    values.pop_back();
+    auto a = std::static_pointer_cast<T>(values.back());
+    values.pop_back();
+
+    if (a->VALUE_TYPE == ValueToken::ASSIGNABLE) {
+        *a = *a / *b;
+        values.push_back(a);
+        return;
+    }
+
+    if (b->VALUE_TYPE == ValueToken::ASSIGNABLE) {
+        *b = *a / *b;
+        values.push_back(b);
+        return;
+    }
+
+    values.push_back(std::make_shared<T>(*a / *b));
+}
+
+template <>
+void divide__<IntegerToken>(ValueStack& values) {
+    auto b = std::static_pointer_cast<IntegerToken>(values.back());
+    values.pop_back();
+    auto a = std::static_pointer_cast<IntegerToken>(values.back());
+    values.pop_back();
+
+    if (*a % *b == 0) {
+        if (a->VALUE_TYPE == ValueToken::ASSIGNABLE) {
+            *a = *a / *b;
+            values.push_back(a);
+            return;
+        }
+
+        if (b->VALUE_TYPE == ValueToken::ASSIGNABLE) {
+            *b = *a / *b;
+            values.push_back(b);
+            return;
+        }
+
+        values.push_back(std::make_shared<IntegerToken>(*a / *b));
+        return;
+    }
+
+    long long valA = *a;
+
+    values.push_back(std::make_shared<FloatToken>((long double)valA / *b));
+}
+
+template <typename Tl, typename Tm>
+void divide__lm(ValueStack& values) {
+    auto b = std::static_pointer_cast<Tm>(values.back());
+    values.pop_back();
+    auto a = std::static_pointer_cast<Tl>(values.back());
+    values.pop_back();
+
+    if (b->VALUE_TYPE == ValueToken::ASSIGNABLE) {
+        *b = *a / *b;
+        values.push_back(b);
+        return;
+    }
+
+    values.push_back(std::make_shared<Tm>(*a / *b));
+}
+
+template <typename Tm, typename Tl>
+void divide__ml(ValueStack& values) {
+    auto b = std::static_pointer_cast<Tl>(values.back());
+    values.pop_back();
+    auto a = std::static_pointer_cast<Tm>(values.back());
+    values.pop_back();
+
+    if (a->VALUE_TYPE == ValueToken::ASSIGNABLE) {
+        *a = *a / *b;
+        values.push_back(a);
+        return;
+    }
+
+    values.push_back(std::make_shared<Tm>(*a / *b));
+}
 }  // namespace ops
 
 namespace {
@@ -209,12 +294,18 @@ void initBasePackage(Package&, const Settings&) {
     times->bind(pats::int_float, ops::multiply__lm<IntegerToken, FloatToken>);
     times->bind(pats::float_int, ops::multiply__ml<FloatToken, IntegerToken>);
     times->bind(pats::float_float, ops::multiply__<FloatToken>);
+
+    divide->bind(pats::int_int, ops::divide__<IntegerToken>);
+    divide->bind(pats::int_float, ops::divide__lm<IntegerToken, FloatToken>);
+    divide->bind(pats::float_int, ops::divide__ml<FloatToken, IntegerToken>);
+    divide->bind(pats::float_float, ops::divide__<FloatToken>);
 }
 
 void preloadBasePackage(Package& p, const Settings&) {
     p.addOperator(base_func::plus);
     p.addOperator(base_func::minus);
     p.addOperator(base_func::times);
+    p.addOperator(base_func::divide);
 }
 
 }  // namespace
